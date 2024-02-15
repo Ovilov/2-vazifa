04529c417799bbbb56454fffd8fcb227b5feed80
Ovilov <ovilovsaidakbar@gmail.com>
2024-02-15 08:13:12 +0500

    2-vazifa add

---
+++ app.js
@@ -0,0 +1,49 @@
+let ism = "Saidakbar";
+let yosh = 22;
+console.log(ism);
+console.log(yosh);
+
+//
+
+let message;
+message = "salom";
+console.log(message);
+message = "hayr";
+console.log(message);
+
+// const a = 5;
+// a = 8;
+// console.log(a); //bu xato !!!
+
+let y = 20;
+x = y;
+console.log(x);
+x = 10;
+console.log(y, x);
+
+//
+
+console.error("xatolik");
+console.warn("ogohlantirish!");
+
+console.group();
+console.log(1);
+console.log(2);
+console.log(3);
+console.groupEnd;
+
+//
+
+alert("Hello World");
+
+//
+
+document.write("Salom,bugun mening tug'ilgan kunim!");
+
+//
+
+let name = "Axad";
+let nickname = "Qayum";
+
+let fullname = `${name} ${nickname}`;
+console.log(fullname);
